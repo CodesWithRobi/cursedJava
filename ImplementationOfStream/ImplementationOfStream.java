363ec48a403fead1497e3eae7c3cfe61b3c52fa1
CodesWithRobi <rayercodes@gmail.com>
2025-10-25 09:00:44 +0530

    ImplementationOfStream

--- ImplementationOfStream/ImplementationOfStream.java
+++ ImplementationOfStream/ImplementationOfStream.java
@@ -1,18 +1,23 @@
 import java.util.Arrays;
 import java.util.IntSummaryStatistics;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class Exp {
+//This bad boy contains the implementations of Stream!!
+import java.util.stream.ReferencePipeline;
+
+public class ImplementationOfStream {
   public static void main(String[] args) {
 
     List<Integer> numbers = Arrays.asList(3, 2, 2, 3, 7, 3, 5);
 
     IntSummaryStatistics stats = numbers.stream().mapToInt((x) -> x).summaryStatistics();
-    ReferencePipeline
+                              // Stream<Integer> -> IntStream (primitive types)
 
     System.out.println("Highest number in List : " + stats.getMax());
     System.out.println("Lowest number in List : " + stats.getMin());
     System.out.println("Sum of all numbers : " + stats.getSum());
     System.out.println("Average of all numbers : " + stats.getAverage());
+
   }
 }
