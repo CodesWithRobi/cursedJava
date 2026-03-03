5b35dda6af23779029dcaecd7eb81c1bde01099b
CodesWithRobi <rayercodes@gmail.com>
2026-03-03 14:21:04 +0530

    TryWithResource

---
+++ TryWithResource/TryWithResource.java
@@ -0,0 +1,15 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+void main() {
+  IO.print("Output file name:");
+  try(Scanner sc = new Scanner(System.in);
+      FileWriter fw = new FileWriter(sc.next());
+      PrintWriter out = new PrintWriter(fw)
+  ) {
+    out.println("Bro all these classes implemented AutoClosables and this try-with-resource call em automatically");
+  } catch (IOException e) {
+    e.printStackTrace();
+  }
+}
