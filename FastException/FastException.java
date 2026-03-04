be48d2503f197a0ce90d1413650babd9ee74d211
CodesWithRobi <rayercodes@gmail.com>
2026-03-04 15:51:33 +0530

    Overriding fillInStackTrace for speeeeed

---
+++ FastException/FastException.java
@@ -0,0 +1,24 @@
+class FastException extends RuntimeException{
+
+  @Override
+  public Throwable fillInStackTrace() {
+    //bypassing the native C++ code that walks the thread stack saving massive amounts of CPU cycles
+    //but since the Throwable() would be calling to fill the stack trace this is only then be used as flag to execute some code..
+    return this;
+  }
+}
+
+void main() {
+  try {
+    throw new RuntimeException();
+  } catch(Exception e) {
+    System.out.println("Normal Exception:");
+    e.printStackTrace();
+  }
+  try {
+    throw new FastException();
+  } catch(Exception e) {
+    System.out.println("-".repeat(50) + "\nFast Exception:");
+    e.printStackTrace();
+  }
+}
