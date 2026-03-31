db9c5d65f99b1b2e6d97ff7ff166606f79eea7a9
CodesWithRobi <rayercodes@gmail.com>
2026-03-31 09:23:05 +0530

    What if two join()?

--- ThreadJoin/ThreadJoin.java
+++ ThreadJoin/ThreadJoin.java
@@ -1,19 +1,27 @@
 public class ThreadJoin {
     public static void main(String[] args) { // <-- Thread A (The Manager)
         
-        Thread worker = new Thread(() -> {   // <-- Thread B (The Worker)
+        Thread worker1 = new Thread(() -> {   // <-- Thread B1 (The Worker1)
+            System.out.println("Worker: Processing data for 3 seconds...");
+            try { Thread.sleep(3000); } catch (Exception e) {}
+            System.out.println("Worker: Done!");
+        });
+        Thread worker2 = new Thread(() -> {   // <-- Thread B1 (The Worker1)
             System.out.println("Worker: Processing data for 3 seconds...");
             try { Thread.sleep(3000); } catch (Exception e) {}
             System.out.println("Worker: Done!");
         });
 
-        worker.start(); // Both threads are now running simultaneously!
+        worker1.start(); // Both threads are now running simultaneously!
+        worker2.start(); // All threads are now running simultaneously!
 
         System.out.println("Manager: Waiting for the worker to finish...");
         
         try {
             // THE CRITICAL LINE
-            worker.join(); 
+            worker1.join(); // Manager(main) is stoped, but worker 2 is not
+            worker2.join(); // worker1 died, if worker2 hasn't finished yet that is running main is paused
+            //I don't care who finishes first, but you are not allowed to move past this line until BOTH are in the grave.
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
